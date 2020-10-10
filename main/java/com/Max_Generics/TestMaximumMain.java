96f51f489c77a92eb06cfe996f7de89876f4e649
suyash-dce <jain.suyash28@gmail.com>
2020-10-10 11:38:51 +0530

    Added: Main Function for Checking Maximum Integer

---
+++ main/java/com/Max_Generics/TestMaximumMain.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class TestMaximumMain {
+	
+	public int maxNumber(Stream<Integer> stream) {
+		return stream.max(Integer::compareTo).get();
+	}
+
+	public static void main( String[] args ) {
+		
+		TestMaximumMain buildObj = new TestMaximumMain();
+    	Scanner sc= new Scanner(System.in);
+    	System.out.println("Input 3 integers: ");
+    	System.out.print("Int 1: ");
+    	int num1 = sc.nextInt();
+    	System.out.print("Int 2: ");
+    	int num2 = sc.nextInt();
+    	System.out.print("Int 3: ");
+    	int num3 = sc.nextInt();
+    	
+    	Integer num_arr[] = new Integer[]{num1,num2,num3};
+    	Stream<Integer> stream=Arrays.stream(num_arr);
+    	System.out.println("Maximum integer = "+buildObj.maxNumber(stream));
+    	sc.close();
+    }
+}
